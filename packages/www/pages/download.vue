01e9cd7aef6f91788ee35a4273a7e0e5ed06c12f
Tristan Ross <rosscomputerguy@protonmail.com>
2021-02-18 21:54:22 -0800

    Ready for test release 1

--- packages/www/pages/download.vue
+++ packages/www/pages/download.vue
@@ -3,8 +3,8 @@
 		<v-row align="center">
 			<v-col cols="12">
 				<v-card elevation="2">
-					<v-card-title>Sorry</v-card-title>
-					<v-card-text>Good news! ExpidusOS Willamette v0.1.0-prealpha test release 1 is expected to be released at the end of February. Please check our Discord for more information and updates.</v-card-text>
+					<v-card-title>Download</v-card-title>
+					<v-card-text><a href="https://build.expidusos.com/0.1.0-prealpha">v0.1.0-prealpha</a> builds can be downloaded from here.</v-card-text>
 				</v-card>
 			</v-col>
 		</v-row>
