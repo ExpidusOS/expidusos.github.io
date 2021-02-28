e5b95af27e0ed8dc213683187554f31671f357ff
Anders Evenrud <andersevenrud@gmail.com>
2021-02-28 03:11:22 +0100

    api: added error log to bootstrap catch

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,10 @@
 import Server from './server'
+import winston from './providers/winston'
 
 const server = new Server()
 
-server.start().then(() => {}).catch(err => {
+server.start().then(() => {}).catch((err) => {
+	winston.error(`Bootstrap error: ${err.stack}`)
 	process.exit(1)
 })
 
