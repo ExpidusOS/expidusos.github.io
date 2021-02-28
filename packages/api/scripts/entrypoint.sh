6405e411af1c2485051f1ba344766677e9a7fa16
Anders Evenrud <andersevenrud@gmail.com>
2021-02-27 18:26:41 -0800

    api: remove webpack in favor of TS environment (#3)
    * Removed webpack setup entirely
    * Now using ts-node-dev as development runtime
    * Added a custom entrypoint for api
    * NODE_ENV is now "development" by default
    * No more bundles, which means that you get real stack traces

--- packages/api/scripts/entrypoint.sh
+++ packages/api/scripts/entrypoint.sh
@@ -1,1 +1,10 @@
-../../common/scripts/entrypoint.sh
+#!/usr/bin/env sh
+
+if [ "$NODE_ENV" == "production" ]; then
+	npm install --only=prod --silent
+	npm run build
+	exec npm run start
+else
+	npm install --silent
+	exec npm run dev
+fi
