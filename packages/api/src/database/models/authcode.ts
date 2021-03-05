db5d4b71f3af964bb6a0605a92cb70260d77b992
Tristan Ross <rosscomputerguy@protonmail.com>
2021-03-04 18:28:10 -0800

    api: removed semicolons

--- packages/api/src/database/models/authcode.ts
+++ packages/api/src/database/models/authcode.ts
@@ -1,12 +1,12 @@
 import { Sequelize, Model, DataTypes } from 'sequelize'
 
 export default class AuthCode extends Model {
-	public code!: string;
-	public expires!: Date;
-	public scope!: string;
-	public uuid!: string;
-	public client_id!: string;
-	public redirect!: string;
+	public code!: string
+	public expires!: Date
+	public scope!: string
+	public uuid!: string
+	public client_id!: string
+	public redirect!: string
 
 	static initializeModel(sequelize: Sequelize): Model {
 		return AuthCode.init({
