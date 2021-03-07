f5a8dbc7b45410ee73b8d76dcd7e06c6e2c1785d
Tristan Ross <rosscomputerguy@protonmail.com>
2021-03-06 22:38:36 -0800

    account: add style css

--- packages/account/nuxt.config.js
+++ packages/account/nuxt.config.js
@@ -52,6 +52,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+		'@expidus/common/dist/style.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
