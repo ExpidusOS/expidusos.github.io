2897c824e317c35b89423962b5d7401a5ae5c5d5
Tristan Ross <tristan.ross@midstall.com>
2023-06-15 17:49:51 -0700

    refactor(home): use adaptive scaffold

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,21 +1,18 @@
+import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:libtokyo_flutter/libtokyo.dart';
 import 'package:expidus_website/layouts.dart';
 import 'package:expidus_website/widgets.dart';
-import 'package:expidus_website/utils.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({ super.key });
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) =>
-    DefaultLayout(
-      child: ListView(
-        padding: const EdgeInsets.all(16.0),
+  Widget _buildMainContent(BuildContext context, {
+    required double horizPadding,
+    required SliverGridDelegate gridDelegate
+  }) =>
+    SingleChildScrollView(
+      child: Column(
         children: [
           Container(
             height: MediaQuery.of(context).size.height / 2.95,
@@ -30,105 +27,146 @@ class _HomePageState extends State<HomePage> {
               child: SimpleCard(
                 children: [
                   Text(
-                    'Introducing ExpidusOS',
+                    AppLocalizations.of(context)!.homeHeadingCardTitle,
                     style: Theme.of(context).textTheme.headlineLarge,
                   ),
                   Text(
-                    'A modern operating system for mobile and desktop devices.',
+                    AppLocalizations.of(context)!.homeHeadingCardBlurb,
                     style: Theme.of(context).textTheme.bodyLarge,
                   ),
                 ],
               ),
             ),
           ),
-          Center(
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                'Features',
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: horizPadding,
+              vertical: 8.0
             ),
-          ),
-          Column(
-            children: <Widget>[
-              SimpleCard(
-                margin: EdgeInsets.all(8.0),
-                children: [
-                  Text(
-                    'Cross Platform',
-                    style: Theme.of(context).textTheme.headlineLarge,
+            child: Column(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      AppLocalizations.of(context)!.homeSectionFeaturesTitle,
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
                   ),
-                  RichText(
-                    text: TextSpan(
+                ),
+                GridView(
+                  gridDelegate: gridDelegate,
+                  shrinkWrap: true,
+                  children: [
+                    SimpleCard(
+                      margin: const EdgeInsets.all(8.0),
                       children: [
-                        TextSpan(
-                          text: 'Designed for more than just your laptop or desktop, ExpidusOS can be used on phones and tablets. ',
-                          style: Theme.of(context).textTheme.bodyLarge,
+                        Text(
+                          AppLocalizations.of(context)!.homeSectionFeaturesCardCrossPlatformTitle,
+                          style: Theme.of(context).textTheme.headlineLarge,
                         ),
-                        TextSpan(
-                          text: 'We plan on supporting a large range of devices, even handheld game consoles. ',
-                          style: Theme.of(context).textTheme.bodyLarge,
-                        ),
-                        TextSpan(
-                          text: 'You can view our ',
-                          style: Theme.of(context).textTheme.bodyLarge,
-                        ),
-                        WidgetSpan(
-                          alignment: PlaceholderAlignment.baseline,
-                          baseline: TextBaseline.alphabetic,
-                          child: TextLink(
-                            Uri.parse('https://wiki.expidusos.com/devices'),
-                            'wiki',
-                            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                              color: Theme.of(context).colorScheme.primary,
-                              fontWeight: FontWeight.bold,
-                              decoration: TextDecoration.underline,
-                            ),
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: AppLocalizations.of(context)!.homeSectionFeaturesCardCrossPlatformContent1,
+                                style: Theme.of(context).textTheme.bodyLarge,
+                              ),
+                              TextSpan(
+                                text: AppLocalizations.of(context)!.homeSectionFeaturesCardCrossPlatformContent2,
+                                style: Theme.of(context).textTheme.bodyLarge,
+                              ),
+                              TextSpan(
+                                text: AppLocalizations.of(context)!.homeSectionFeaturesCardCrossPlatformContent3,
+                                style: Theme.of(context).textTheme.bodyLarge,
+                              ),
+                              WidgetSpan(
+                                alignment: PlaceholderAlignment.baseline,
+                                baseline: TextBaseline.alphabetic,
+                                child: TextLink(
+                                  Uri.parse('https://wiki.expidusos.com/devices'),
+                                  AppLocalizations.of(context)!.homeSectionFeaturesCardCrossPlatformContent4,
+                                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                    color: Theme.of(context).colorScheme.primary,
+                                    fontWeight: FontWeight.bold,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ),
+                              TextSpan(
+                                text: AppLocalizations.of(context)!.homeSectionFeaturesCardCrossPlatformContent5,
+                                style: Theme.of(context).textTheme.bodyLarge,
+                              ),
+                            ],
+                            style: Theme.of(context).textTheme.bodyLarge,
                           ),
                         ),
-                        TextSpan(
-                          text: ' for more information on our currently supported devices and one\'s we plan on supporting in the future.',
+                      ],
+                    ),
+                    SimpleCard(
+                      margin: const EdgeInsets.all(8.0),
+                      children: [
+                        Text(
+                          AppLocalizations.of(context)!.homeSectionFeaturesCardFOSSTitle,
+                          style: Theme.of(context).textTheme.headlineLarge,
+                        ),
+                        Text(
+                          AppLocalizations.of(context)!.homeSectionFeaturesCardFOSSContent,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        )
+                      ],
+                    ),
+                    SimpleCard(
+                      margin: const EdgeInsets.all(8.0),
+                      children: [
+                        Text(
+                          AppLocalizations.of(context)!.homeSectionFeaturesCardEasyUseCardTitle,
+                          style: Theme.of(context).textTheme.headlineLarge,
+                        ),
+                        Text(
+                          AppLocalizations.of(context)!.homeSectionFeaturesCardEasyUseCardContent,
                           style: Theme.of(context).textTheme.bodyLarge,
                         ),
                       ],
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                  ),
-                ],
-              ),
-              SimpleCard(
-                margin: EdgeInsets.all(8.0),
-                children: [
-                  Text(
-                    'Free & Open Source',
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                  Text(
-                    'The source code of ExpidusOS is open source and the operating system itself is free. You can download it from this website or build it yourself. ExpidusOS also uses open source technologies like Linux, Zig, Flutter, and Nix.',
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                ],
-              ),
-              SimpleCard(
-                margin: EdgeInsets.all(8.0),
-                children: [
-                  Text(
-                    'Easy to use',
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                  Text(
-                    'ExpidusOS uses modern design technologies to adapt its layout between mobile and desktop devices. This means you can enjoy the same experience on your phone and desktop with ExpidusOS.',
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                ],
-              ),
-            ].map((w) => AutoScaler.ltLarge.fits(MediaQuery.of(context)) ? w : SizedBox(
-              width: MediaQuery.of(context).size.width / 1.6,
-              child: w,
-            )).toList(),
+                    )
+                  ].map((child) => Expanded(child: child)).toList(),
+                ),
+              ],
+            ),
           ),
         ],
+      ),
+    );
+
+  @override
+  Widget build(BuildContext context) =>
+    DefaultLayout(
+      child: AdaptiveLayout(
+        internalAnimations: false,
+        body: SlotLayout(
+          config: <Breakpoint, SlotLayoutConfig>{
+            Breakpoints.smallAndUp: SlotLayout.from(
+              key: const Key('primaryMobile'),
+              builder: (_) => _buildMainContent(
+                context,
+                horizPadding: 8.0,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 1,
+                )
+              ),
+            ),
+            Breakpoints.large: SlotLayout.from(
+              key: const Key('primaryBody'),
+              builder: (_) => _buildMainContent(
+                context,
+                horizPadding: MediaQuery.sizeOf(context).width / 4,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+              ),
+            ),
+          },
+        )
       ),
     );
 }
